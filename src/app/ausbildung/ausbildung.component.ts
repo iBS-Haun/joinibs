9de681a6773cb97771a259442d80754dfcf9ba63
Marcel Mehl <marcel.mehl@ibs-banking.com>
2021-08-16 14:19:00 +0200

    changed Bewerbung-Component to fit multiple pages and general styling updates

--- src/app/ausbildung/ausbildung.component.ts
+++ src/app/ausbildung/ausbildung.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ausbildung',
   templateUrl: './ausbildung.component.html',
-  styleUrls: ['./ausbildung.component.css']
+  styleUrls: ['./ausbildung.component.css'],
 })
 export class AusbildungComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  linkToApplicationAusbildung: string =
+    'https://www.ibs-banking.com/de/unternehmen/karriere/job-angebote/88-ausbildung-zum-fachinformatiker-anwendungsentwicklung.html?ls=JoinMicroSitehttps://www.ibs-banking.com/de/unternehmen/karriere/job-angebote/88-ausbildung-zum-fachinformatiker-anwendungsentwicklung.html?ls=JoinMicroSite';
+  ngOnInit(): void {}
 }
