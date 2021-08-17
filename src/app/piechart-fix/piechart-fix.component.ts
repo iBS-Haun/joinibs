a3b804ca77b5accc394c3df5461868340f553850
Marcel Mehl <marcel.mehl@ibs-banking.com>
2021-08-17 14:59:55 +0200

    cards components should extend properly. Added a piechart to Studium and changed it's content

--- src/app/piechart-fix/piechart-fix.component.ts
+++ src/app/piechart-fix/piechart-fix.component.ts
@@ -1,33 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
 import { MultiDataSet, Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-piechart-fix',
   templateUrl: './piechart-fix.component.html',
-  styleUrls: ['./piechart-fix.component.css']
+  styleUrls: ['./piechart-fix.component.css'],
 })
 export class PiechartFixComponent implements OnInit {
-
-  ChartLabels: Label[] = ['Praxis', 'DXC-Seminare', 'Berufsschule'];
-  ChartData: MultiDataSet = [
-    [50, 20, 30]
-  ];
+  @Input() ChartLabels: Label[] = ['Praxis', 'DXC-Seminare', 'Berufsschule'];
+  @Input() ChartData: MultiDataSet = [[50, 20, 30]];
   ChartType: ChartType = 'doughnut';
+  @Input() headline: string = 'Deine Ausbildung bei der iBS AG';
 
-  colors: Color [] = [
+  @Input() colors: Color[] = [
     {
-      backgroundColor: [
-        '#03a9f4',
-        '#d1ebf0',
-        '#282f57'
-      ]
-    }
+      backgroundColor: ['#03a9f4', '#d1ebf0', '#282f57'],
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/studium/studium.component.ts
+++ src/app/studium/studium.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-studium',
@@ -10,6 +11,13 @@ export class StudiumComponent implements OnInit {
     'https://ibs-banking.softgarden.io/applications/d9b507ec-b6d4-4305-83d1-e99102684b54?7&isEditMode=false&isNew=true&l=de';
 
   constructor() {}
+  chartLabels: Label[] = ['Paxis', 'Vorlesungen'];
+  chartData: MultiDataSet = [[50, 50]];
+  chartColors: Color[] = [
+    {
+      backgroundColor: ['#009EE0', '#eeeeee'],
+    },
+  ];
 
   ngOnInit(): void {}
 }
